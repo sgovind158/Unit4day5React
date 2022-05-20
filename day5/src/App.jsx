5073bbe43581d4e798242bc71d9649e5531b2d8c
Govind Sahu <govindsahu2842@gmail.com>
2022-05-20 22:32:20 +0530

    day5

---
+++ day5/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Navbar from './Componets/Navbar';
+
+function App() {
+  return (
+    <div >
+    <Navbar/>
+    </div>
+  );
+}
+
+
+// https://raw.githubusercontent.com/riteshf/cherry/main/assets/offer1.jpeg
+export default App;
